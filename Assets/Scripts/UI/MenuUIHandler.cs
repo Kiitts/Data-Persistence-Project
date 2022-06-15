b5d515d3383f4fd4c0ef91c5131022494e2e38aa
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-06-16 00:39:59 +0800

    Play & Quit Button are now working in main menu

--- Assets/Scripts/UI/MenuUIHandler.cs
+++ Assets/Scripts/UI/MenuUIHandler.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using TMPro;
 
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField playerName;
+
+    private void Start() 
     {
-        
+        playerName.text = "Player";
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit();
+        #endif
     }
 }
